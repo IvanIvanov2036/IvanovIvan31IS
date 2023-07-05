a9f723934541f80e699341548aefb92db51d06e8
316de <316de@315-DE-21>
2023-07-05 09:40:44 +0300

    Добавьте файлы проекта.

---
+++ IvanovIvan31IS/Class.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IvanovIvan31IS
+{
+    public class Building
+    {
+        public string Address {get; set;}
+        public string Description {get; set;}
+        public List<Apartment> Apartments {get; set;}
+
+        public Building(string address, string description)
+        {
+            Address = address;
+            Description = description;
+            Apartments = new List<Apartment>();
+        }
+
+        public void AddApartment(Apartment apartment)
+        {
+            Apartments.Add(apartment);
+        }
+
+        public void Show()
+        {
+            Console.WriteLine($"Address: {Address}");
+            Console.WriteLine($"Description: {Description}");
+        }
+
+        public void ShowAll()
+        {
+            Apartments.Sort(); // сортируем квартиры по номеру
+            foreach (Apartment apartment in Apartments)
+            {
+                apartment.Show();
+            }
+        }
+    }
+
+    public class Apartment : IComparable<Apartment>
+    {
+        public int Number { get; set; }
+        public string Owner { get; set; }
+
+        public Apartment(int number, string owner)
+        {
+            Number = number;
+            Owner = owner;
+        }
+
+        public void Show()
+        {
+            Console.WriteLine($"Apartment number: {Number}, owner: {Owner}");
+        }
+
+        public int CompareTo(Apartment other)
+        {
+            return Number.CompareTo(other.Number);
+        }
+    }
+}
